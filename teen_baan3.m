3980504c1c648cca81ee6ee30f6484def26d0a19
Juvith <49005861+Juvith@users.noreply.github.com>
2019-03-28 20:17:41 +0530

    Delete teen_baan3.m

--- teen_baan3.m
+++
@@ -1,6 +0,0 @@
-filepath = fullfile(matlabroot,'examples','robotics','imageMap.png');
-image = imread(filepath);
-grayimage = rgb2gray(image);
-bwimage = grayimage < 0.5;
-grid = robotics.BinaryOccupancyGrid(bwimage);
-show(grid)
